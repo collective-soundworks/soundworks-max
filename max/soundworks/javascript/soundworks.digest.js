9f81c63cb8c57b18b3947570bfd07bcefb12a57b
b-ma <benjamin.matuszewski@gmail.com>
2022-09-23 14:35:31 +0200

    cleaning old files

--- max/soundworks/javascript/soundworks.digest.js
+++ max/soundworks/javascript/soundworks.digest.js
@@ -1,22 +1,21 @@
-inlets=1;
-outlets=1;
+inlets = 1;
+outlets = 1;
 
-function anything()
-{
+function anything() {
 	var a = arrayfromargs(messagename, arguments);
 
 	var shortDesc = "";
 	var longDesc = "";
 
-	dict = max.getrefdict( a[0] );
-	if( typeof(dict) == "object" )
-	{
+	dict = max.getrefdict(a[0]);
+
+	if (typeof dict == "object") {
 		shortDesc = dict.get("digest");
 		longDesc = dict.get("description");
 		dict.freepeer();
 	}
 	
-	outlet( 0 , "digest", shortDesc );
-	outlet( 0 , "description", longDesc );
+	outlet(0 , "digest", shortDesc);
+	outlet(0 , "description", longDesc );
 }
 
